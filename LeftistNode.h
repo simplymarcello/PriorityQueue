59a4c541311b92d3dcbaf5c7ceff5db54e390b63
simplymarcello <simplymarcello@gmail.com>
2016-03-08 11:53:20 -0500

    Init Commit

---
+++ LeftistNode.h
@@ -0,0 +1,38 @@
+// Created by Marcello Martins on Nov 18, 2015
+// Last modified on Nov 20, 2015
+// LeftistNode Header and implementation file
+
+/** @file LeftistNode.h */
+
+#ifndef LEFTIST_NODE_H
+#define LEFTIST_NODE_H
+
+// LeftistNode class for PriorityQueue
+//
+// CONSTRUCTION: best with no parameters
+//
+// ******************MAIN OPERATIONS***********************
+// 
+// ********************************************************
+
+class LeftistNode
+{
+public:
+    int         element;
+    LeftistNode *left;
+    LeftistNode *right;
+    LeftistNode *parent;
+    int          npl;
+
+    LeftistNode( const int & e, LeftistNode *lt = nullptr,
+                    LeftistNode *rt = nullptr, int np = 0, LeftistNode *pt = nullptr )
+      : element{ e }, left{ lt }, right{ rt }, parent{ pt }, npl{ np }  { }
+    
+    LeftistNode( int && e, LeftistNode *lt = nullptr,
+                    LeftistNode *rt = nullptr, int np = 0,  LeftistNode *pt = nullptr  )
+      : element{ std::move( e ) }, left{ lt }, right{ rt }, parent{ pt }, npl{ np } { }
+
+};
+
+
+#endif
